21a74e99618cf954dbc34b105e1c27a91648b4ce
Vinicius Flores <fzvinicius@gmail.com>
2013-07-19 01:42:33 -0300

    ok

--- web.js
+++ web.js
@@ -1,12 +1,23 @@
 var express = require('express');
+var fs = require('fs');
+
 var app = express();
 app.use(express.logger());
 
+
+var data = new Buffer(128);
+data = fs.readFileSync('index.html');
+index = data.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    response.send(index);
 });
+
+/*app.get('/', function(request, response) {
+    response.send('Hello World2!');
+});*/
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
